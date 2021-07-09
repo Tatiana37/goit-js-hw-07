fe44e216599fb6097a35c27ec8288c249e3bff72
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-07-09 23:23:08 +0300

    добавила решение 2й задачи

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const list = document.querySelector('#ingredients')
+console.log(list);
+
+const ingredientsList = ingredients.map(element => {
+  const item = document.createElement('li');
+  item.append(element);
+  console.log(item);
+  return item;
+  
+});
+
+list.append(...ingredientsList);
+
+
+
