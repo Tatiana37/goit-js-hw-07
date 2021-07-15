67403aff97484a15e70849e820d4cbb31c95c57b
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-07-16 00:06:07 +0300

    добавила задачу 5

--- js/task-05.js
+++ js/task-05.js
@@ -4,3 +4,28 @@
 
 // <input type="text" placeholder="Ваше имя?" id="name-input" />
 // <h1>Привет, <span id="name-output">незнакомец</span>!</h1>
+
+
+const refs = {
+    input: document.querySelector('#name-input'),
+    spanName: document.querySelector('#name-output'),
+}
+
+// console.log(refs.input);
+// console.log(refs.spanName);
+
+// refs.input.addEventListener('input', onInputChange);
+
+// function onInputChange(event) {
+//     console.log(event.currentTarget.value);
+//     refs.spanName.textContent = event.currentTarget.value === ''
+//         ? 'незнакомец'
+//         : event.currentTarget.value;
+// }
+
+refs.input.addEventListener('input', event => {
+    refs.spanName.textContent = event.currentTarget.value === ''
+        ? 'незнакомец'
+        : event.currentTarget.value;
+});
+
